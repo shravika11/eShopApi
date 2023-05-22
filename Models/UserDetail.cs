3d8f5c4f3a6d417549b25cc6904f8220884d06b8
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:28:54 +0530

    Add files via upload

---
+++ Models/UserDetail.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace eShopApi.Models
+{
+    public class UserDetail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        [DataType("varchar(30)")]
+        [Required(ErrorMessage = "Email can not be empty")]
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage = "Mobile no. can not be empty")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number should be a 10-digit number.")]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        public string AddressInfo { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string UserState { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{6}$", ErrorMessage = "Pincode should be a 6-digit number.")]
+        public string Pincode { get; set; }
+
+        [Required(ErrorMessage = "Password can not be empty")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public bool IsLogin { get; set; } = false;
+    }
+}
