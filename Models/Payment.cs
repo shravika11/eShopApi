3a996c086a7a05d124c4f901bf5f4e0e8fab65df
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:27:39 +0530

    Add files via upload

---
+++ Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace eShopApi.Models
+{
+    public class Payment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransactionId { get; set; }
+        public decimal TransactionAmount { get; set; }
+
+        [ForeignKey("UserDetails")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Order")]
+        public int OrderId { get; set; }
+        public string Tstatus { get; set; }
+        public string Mode { get; set; }
+        public string Code { get; set; }
+
+    }
+}
