7bcf7adaa94e15dc4d5963e93ed4e4d6d72dcb92
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:31:43 +0530

    Order.cs

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,1 +1,28 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
+namespace eShopApi.Models
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+        [ForeignKey("Cart")]
+        public int CartId { get; set; }
+
+        [ForeignKey("UserDetails")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductImage { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
