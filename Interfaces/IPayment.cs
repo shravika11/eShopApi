51dee610287bd2f5a1973e15806976b096af93fb
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 09:19:06 +0530

    Add files via upload

---
+++ Interfaces/IPayment.cs
@@ -0,0 +1,15 @@
+﻿using eShopApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eShopApi.Interfaces
+{
+    public interface IPayment
+    {
+        Task<List<Payment>> GetAllTransactionAsync();
+        Task<Payment> GetTransactionAsync(int transactionId);
+        Task<string> SaveTransactionAsync(Payment payment);
+        Task<string> UpdateTransactionAsync(Payment payment);
+        Task<string> DeleteTransactionAsync(int transactionId);
+    }
+}
