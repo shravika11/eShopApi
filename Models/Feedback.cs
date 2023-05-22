3cbd6d8b436dd15b15312775b3daae3258743766
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:26:09 +0530

    Add files via upload

---
+++ Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace eShopApi.Models
+{
+    public class Feedback
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Query { get; set; }
+    }
+}
