4d41cae0d6d4641c7c7707ecd574b01eef5792aa
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:27:06 +0530

    Add files via upload

---
+++ Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eShopApi.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Enter Email")]
+        public string EmailId { get; set; }
+        [Required(ErrorMessage = "Enter Password")]
+        public string Password { get; set; }
+    }
+}
