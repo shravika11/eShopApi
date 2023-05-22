085773362949422afa50bc84f346f09c3b74839d
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:30:58 +0530

    Cart.cs

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,1 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
+namespace eShopApi.Models
+{
+    public class Cart
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CartId { get; set; }
+
+        [ForeignKey("UserDetails")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductImage { get; set; }
+        public int Quantity { get; set; } = 1;
+
+        public decimal Price { get; set; }
+    }
+}
