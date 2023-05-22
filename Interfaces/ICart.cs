660943ede85828e6e30e922b726edce7e5733d3a
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 09:13:24 +0530

    Add files via upload

---
+++ Interfaces/ICart.cs
@@ -0,0 +1,15 @@
+﻿using eShopApi.Models;
+
+namespace eShopApi.Interfaces
+{
+    public interface ICart
+    {
+        Task<string> DeleteCart(int cartId);
+        Task<List<Cart>> GetAllCart();
+        Task<Cart> GetCart(int cartId);
+        Task<string> SaveCart(Cart cart);
+        Task<string> UpdateCart(Cart cart);
+        Task<IEnumerable<Cart>> GetCartByUserID(int UserId);
+        Task<int> GetCartId(int UserId);
+    }
+}
