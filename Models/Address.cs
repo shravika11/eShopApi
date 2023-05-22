7345576b3728a9c9d65c7ed4e6eab69bacf1c6e9
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:30:08 +0530

    Address.cs

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,1 +1,21 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
+namespace eShopApi.Models
+{
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int AddressId { get; set; }
+
+        [ForeignKey("UserDetails")]
+        public int UserId { get; set; }
+        public string AddressInfo { get; set; }
+        public string City { get; set; }
+        public string UserState { get; set; }
+        [RegularExpression(@"^\d{6}$", ErrorMessage = "Pincode should be a 6-digit number.")]
+        public string Pincode { get; set; }
+    }
+}
