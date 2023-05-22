8fee1e6bba24e5ffa32f138290613b30bd402202
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 09:14:27 +0530

    Add files via upload

---
+++ Interfaces/IFeedback.cs
@@ -0,0 +1,12 @@
+﻿using eShopApi.Models;
+
+namespace eShopApi.Interfaces
+{
+    public interface IFeedback
+    {
+        Task<bool> SaveFeedDetailsAsync(Feedback feedback);
+        Task<Feedback> GetFeedDetailsAsync(int Id);
+        Task<List<Feedback>> GetAllFeedDetailsAsync();
+        Task<bool> DeleteFeedDetailsAsync(int Id);
+    }
+}
---
+++ Interfaces/IOrder.cs
@@ -0,0 +1,13 @@
+﻿using eShopApi.Models;
+
+namespace eShopApi.Interfaces
+{
+    public interface IOrder
+    {
+        Task<string> DeleteOrderDetails(int orderId);
+        Task<List<Order>> GetAllOrderDetails();
+        Task<Order> GetOrderDetails(int orderId);
+        Task<string> SaveOrderDetails(Order orderDetails);
+        Task<string> UpdateOrderDetails(Order orderDetails);
+    }
+}
