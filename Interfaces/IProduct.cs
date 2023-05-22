96de556aa396340399da0773c0342d659f46b022
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 09:19:35 +0530

    Add files via upload

---
+++ Interfaces/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using eShopApi.Models;
+
+namespace eShopApi.Interfaces
+{
+    public interface IProduct
+    {
+        Task<List<Product>> GetAllProductsAsync();
+        Task<Product> GetProductAsync(int ProductId);
+        Task<string> SaveProductAsync(Product product);
+        Task<string> UpdateProductAsync(Product product);
+        Task<string> DeleteProductAsync(int ProductId);
+        Task<List<Product>> GetProductsByCategoryAsync(string category);
+    }
+}
