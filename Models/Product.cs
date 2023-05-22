e740bf58c1db18e3cf78704799308e79c14f3df0
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 08:28:21 +0530

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace eShopApi.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        public string Category { get; set; }
+
+        [DataType("varchar(30)")]
+        [Required(ErrorMessage = "Prouct name can not be empty")]
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ProductImage { get; set; }
+    }
+}
