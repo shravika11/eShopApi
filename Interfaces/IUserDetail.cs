d45f1de815be0585d0a841179f238fe961809ffe
shravika11 <124347935+shravika11@users.noreply.github.com>
2023-05-22 09:20:02 +0530

    Add files via upload

---
+++ Interfaces/IUserDetail.cs
@@ -0,0 +1,14 @@
+﻿using eShopApi.Models;
+
+namespace eShopApi.Interfaces
+{
+    public interface IUserDetail
+    {
+        Task<string> SaveUserDetailAsync(UserDetail userDetail);
+        Task<UserDetail> GetUserDetailAsync(int userId);
+        Task<List<UserDetail>> GetAllUserDetailsAsync();
+        Task<string> UpdateUserDetailAsync(UserDetail userDetail);
+        Task<string> DeleteUserDetailAsync(int userId);
+        Task<UserDetail> GetUserByEmailAsync(string emailId);
+    }
+}
